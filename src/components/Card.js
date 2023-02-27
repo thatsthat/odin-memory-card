c820630197e3ded739d04038ba3dc5b81deef503
Aram <aram.anto@gmail.com>
2023-02-27 16:04:10 +0100

    hola

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,19 @@
 import React, { useState, useEffect } from "react";
 import styles from "../styles/Card.module.css";
 
-const Card = () => {
+const Card = (props) => {
   const [flagImg, setFlagImg] = useState();
-  const [flagName, setFlagName] = useState();
 
   useEffect(() => {
     fetchImage();
   }, []);
 
   const fetchImage = async () => {
-    const resp = await fetch("https://flagcdn.com/en/codes.json");
-    const data = await resp.json();
-    const imageUrl = `https://flagcdn.com/${Object.keys(data)[33]}.svg`;
+    const imageUrl = `https://flagcdn.com/${props.code}.svg`;
     const res = await fetch(imageUrl);
     const imageBlob = await res.blob();
     const imageObjectURL = URL.createObjectURL(imageBlob);
     setFlagImg(imageObjectURL);
-    setFlagName(Object.values(data)[33]);
     return URL;
   };
 
@@ -25,7 +21,7 @@ const Card = () => {
     <div>
       <div className={styles.card}>
         <img src={flagImg} width="150" alt="flag" />
-        <div>{flagName}</div>
+        <div>{props.name}</div>
       </div>
     </div>
   );
