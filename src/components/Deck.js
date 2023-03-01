fea0e389ec24afdec99f55e983c88647a639c701
Aram <aram.anto@gmail.com>
2023-03-01 16:54:11 +0100

    pick 12 random flags each time

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -4,12 +4,18 @@ import Card from "./Card";
 
 const Deck = () => {
   const [flagData, setFlagData] = useState();
+  const [flagSequence, setFlagSequence] = useState();
 
   useEffect(() => {
     const fetchList = async () => {
       const resp = await fetch("https://flagcdn.com/en/codes.json");
       const data = await resp.json();
       setFlagData(data);
+      const totalFlags = Object.keys(data).length;
+      const flagSequence = Array(...Array(12)).map((v, i) =>
+        Math.floor(Math.random() * totalFlags)
+      );
+      setFlagSequence(flagSequence);
     };
 
     fetchList();
@@ -17,12 +23,12 @@ const Deck = () => {
 
   return (
     <div className={styles.deck}>
-      {flagData
-        ? Array(...Array(3)).map((v, i) => (
+      {flagData && flagSequence
+        ? Array(...Array(12)).map((v, i) => (
             <Card
               key={i}
-              keyS={Object.keys(flagData)[i]}
-              name={Object.values(flagData)[i]}
+              keyS={Object.keys(flagData)[Object.values(flagSequence)[i]]}
+              name={Object.values(flagData)[Object.values(flagSequence)[i]]}
             />
           ))
         : ""}
