1da13af687a645d26011cc45a9b06220dcf48d5c
Aram <aram.anto@gmail.com>
2023-03-07 16:28:08 +0100

    nothing

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import ScoreBoard from "./components/ScoreBoard";
 function App() {
   const [score, setScore] = useState();
   const [clickedFlags, setClickedFlags] = useState();
+
   useEffect(() => {
     // Initialize clicked flags as an empty array
     setClickedFlags([]);
   }, []);
 
-  const clickCardFunc = (event) =>
-    console.log(event.currentTarget.firstChild.getAttribute("code"));
+  const clickCardFunc = (event) => {
+    const flag = event.currentTarget.firstChild.getAttribute("code");
+    if (clickedFlags.includes(flag)) {
+    }
+  };
+
   return (
     <div className="App">
       <Deck clickCard={clickCardFunc} />
