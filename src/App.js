7f5c3f3105edd4db1ae2b0a4e67dec824a78c3c5
Aram <aram.anto@gmail.com>
2023-03-09 17:20:11 +0100

    works but no reshuffle yet

--- src/App.js
+++ src/App.js
@@ -3,25 +3,88 @@ import Deck from "./components/Deck";
 import ScoreBoard from "./components/ScoreBoard";
 
 function App() {
+  const nFlags = 15;
   const [score, setScore] = useState();
-  const [clickedFlags, setClickedFlags] = useState();
+  const [clickedFlags, setClickedFlags] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [flagSeq, setFlagSeq] = useState();
+  const [flagData, setFlagData] = useState();
+
+  const randFlags = (numFlags, maxInt) => {
+    // You can take this value from user
+    const n = 5;
+
+    // Initial empty array
+    const arr = [];
+
+    // Null check
+    if (n === 0) {
+      console.log(null);
+    }
+
+    do {
+      // Generating random number
+      const randomNumber = Math.floor(Math.random() * maxInt);
+      // Pushing into the array only
+      // if the array does not contain it
+      if (!arr.includes(randomNumber)) {
+        arr.push(randomNumber);
+      }
+    } while (arr.length < numFlags);
+
+    // Printing the array elements
+    return arr;
+  };
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
 
   useEffect(() => {
-    // Initialize clicked flags as an empty array
-    setClickedFlags([]);
+    const fetchImages = async () => {
+      const resp = await fetch("https://flagcdn.com/en/codes.json");
+      const data = await resp.json();
+      const totalFlags = Object.keys(data).length;
+      const flagInts = randFlags(nFlags, totalFlags);
+      const flagSequence = [...Array(nFlags).keys()].map((i) => i);
+      setFlagSeq(flagSequence);
+      const images = Array(...Array(nFlags)).map((v, i) => {
+        const randNum = flagInts[i];
+        const flagCode = Object.keys(data)[randNum];
+        return {
+          url: `https://flagcdn.com/${flagCode}.svg`,
+          code: flagCode,
+          name: Object.values(data)[randNum],
+        };
+      });
+      setFlagData(images);
+    };
+    fetchImages();
   }, []);
 
   const clickCardFunc = (event) => {
+    //setCounter((a) => a + 1);
+    setFlagSeq((seq) => shuffleArray(seq));
     const flag = event.currentTarget.firstChild.getAttribute("code");
     if (clickedFlags.includes(flag)) {
     }
   };
-
-  return (
-    <div className="App">
-      <Deck clickCard={clickCardFunc} />
-    </div>
-  );
+  if (!(typeof flagData === "undefined")) {
+    return (
+      <div className="App">
+        <Deck
+          clickCard={clickCardFunc}
+          key={counter}
+          flagData={flagData}
+          flagSeq={flagSeq}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -3,66 +3,18 @@ import styles from "../styles/Deck.module.css";
 import Card from "./Card";
 
 const Deck = (props) => {
-  const [flagData, setFlagData] = useState();
-
-  useEffect(() => {
-    const randFlags = (numFlags, maxInt) => {
-      // You can take this value from user
-      const n = 5;
-
-      // Initial empty array
-      const arr = [];
-
-      // Null check
-      if (n == 0) {
-        console.log(null);
-      }
-
-      do {
-        // Generating random number
-        const randomNumber = Math.floor(Math.random() * maxInt);
-        // Pushing into the array only
-        // if the array does not contain it
-        if (!arr.includes(randomNumber)) {
-          arr.push(randomNumber);
-        }
-      } while (arr.length < numFlags);
-
-      // Printing the array elements
-      return arr;
-    };
-    const fetchImages = async () => {
-      const resp = await fetch("https://flagcdn.com/en/codes.json");
-      const data = await resp.json();
-      const totalFlags = Object.keys(data).length;
-      const flagInts = randFlags(15, totalFlags);
-      const images = Array(...Array(15)).map((v, i) => {
-        const randNum = flagInts[i];
-        const flagCode = Object.keys(data)[randNum];
-        return {
-          url: `https://flagcdn.com/${flagCode}.svg`,
-          code: flagCode,
-          name: Object.values(data)[randNum],
-        };
-      });
-      setFlagData(images);
-    };
-    fetchImages();
-  }, []);
-
+  console.log(props.flagSeq);
   return (
     <div className={styles.deck}>
-      {flagData
-        ? Array(...Array(15)).map((v, i) => (
-            <Card
-              key={flagData[i].code}
-              image={flagData[i].url}
-              name={flagData[i].name}
-              click={props.clickCard}
-              code={flagData[i].code}
-            />
-          ))
-        : ""}
+      {Array(...Array(15)).map((v, i) => (
+        <Card
+          key={props.flagData[props.flagSeq[i]].code}
+          image={props.flagData[props.flagSeq[i]].url}
+          name={props.flagData[props.flagSeq[i]].name}
+          click={props.clickCard}
+          code={props.flagData[props.flagSeq[i]].code}
+        />
+      ))}
     </div>
   );
 };
