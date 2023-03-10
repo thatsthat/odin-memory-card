7938a6672cca491a0443a19e91209192bcb70c5f
Aram <aram.anto@gmail.com>
2023-03-10 11:19:00 +0100

    add link to flagpedia

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -4,16 +4,21 @@ import Card from "./Card";
 
 const Deck = (props) => {
   return (
-    <div className={styles.deck}>
-      {Array(...Array(15)).map((v, i) => (
-        <Card
-          key={props.flagData[props.flagSeq[i]].code}
-          image={props.flagData[props.flagSeq[i]].url}
-          name={props.flagData[props.flagSeq[i]].name}
-          click={props.clickCard}
-          code={props.flagData[props.flagSeq[i]].code}
-        />
-      ))}
+    <div>
+      <div className={styles.deck}>
+        {Array(...Array(15)).map((v, i) => (
+          <Card
+            key={props.flagData[props.flagSeq[i]].code}
+            image={props.flagData[props.flagSeq[i]].url}
+            name={props.flagData[props.flagSeq[i]].name}
+            click={props.clickCard}
+            code={props.flagData[props.flagSeq[i]].code}
+          />
+        ))}
+      </div>
+      <div className={styles.link}>
+        <a href="https://flagpedia.net">Built on Flagpedia API</a>
+      </div>
     </div>
   );
 };
